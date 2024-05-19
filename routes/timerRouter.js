6179169f24a58972fc2641f334f89689432ea1dd
Chadri <114363325+chadri0@users.noreply.github.com>
2024-05-18 20:33:43 -0700

    modified timerRouter

--- routes/timerRouter.js
+++ routes/timerRouter.js
@@ -5,25 +5,25 @@ const {startTimer, stopTimer, resetTimer, getState} = require("../controllers/ti
 
 // default timer state route
 router.get("/timer", async (request, response, next) => {
-    response.status(200).json(getState());
+    response.json(getState());
 });
 // router.get("/<route>", <variable name>); //doesn't work for this
 
 // timer start route
 router.post("/timer/start", async (request, response, next) => {
     startTimer();
-    response.status(200).json({ success: "Timer started"});
+    response.json({ success: "Timer started"});
 });
 // timer pause route
 router.delete("/timer/pause", async (request, response, next) => {
     stopTimer();
-    response.status(200).json({ success: "Timer stopped"});
+    response.json({ success: "Timer stopped"});
 });
 
 // timer reset route
 router.put("/timer/reset", async (request, response, next) => {
     resetTimer();
-    response.status(200).json({ success: "Timer reset"});
+    response.json({ success: "Timer reset"});
 });
 
 //export
